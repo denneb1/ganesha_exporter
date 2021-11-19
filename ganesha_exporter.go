5534f5c664b08c461e73a498a8bbcf21bd7ba448
denneb1 <30631134+denneb1@users.noreply.github.com>
2021-11-19 15:04:40 +0100

    Update ganesha_exporter.go

--- ganesha_exporter.go
+++ ganesha_exporter.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/Gandi/ganesha_exporter/dbus"
+	"github.com/denneb1/ganesha_exporter/dbus"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
